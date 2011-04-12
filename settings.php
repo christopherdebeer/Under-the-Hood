4573db83c44377444bba85603c55e263603bcd20
Chrsitopher de Beer <christopherdebeer@gmail.com>
2011-04-12 17:53:10 +0100

    removed gallery form index and placed in core/gallery.php and further explained gallery vars in settings.php

--- settings.php
+++ settings.php
@@ -13,8 +13,14 @@ $app_title = "Under the Hood";
 
 $prefered_order_of_pages = array("Home.html","The_Event.html","The_Brief.html","Gallery.html");
 
+// if you would like underthehood to auto generate a gallery page from a flickr account hten set the bellow value to true
+// and supply the relevant usid  below that.
+// please note: if you want an auto galery, you NEED to specify "Gallery.html" in the $prefered_order_of_pages above,
+// this will be doen automagically in a later build but is not yet implemented.
 
-$autogenerate_gallery = true;
+$autogenerate_gallery = false;
+
+// enter the usid (flickr user id) of the user accoutn from whence you'd like to fetch images
 
 $flickr_nsid = '88399767@N00';
 
